120d4d9a070f9bd2399ed926074d175fb11f9666
RussianJudge <adambursey@hotmail.com>
2026-05-29 11:03:35 -0600

    Make workspace shell full height

--- components/monthly-scheduler.tsx
+++ components/monthly-scheduler.tsx
@@ -2202,8 +2202,19 @@ export function MonthlyScheduler({
         </div>
         <div className="planner-actions">
           <div className="planner-actions__row planner-actions__row--save">
-            <button type="button" className="ghost-button" onClick={handlePrintSchedules}>
-              Print schedules
+            <button
+              type="button"
+              className="icon-button"
+              onClick={handlePrintSchedules}
+              aria-label="Print schedules"
+              title="Print schedules"
+            >
+              <svg viewBox="0 0 24 24" aria-hidden="true">
+                <path d="M7 8V3h10v5" />
+                <path d="M7 17H5a2 2 0 0 1-2-2v-4a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v4a2 2 0 0 1-2 2h-2" />
+                <path d="M7 14h10v7H7z" />
+                <path d="M17 11h.01" />
+              </svg>
             </button>
           </div>
         </div>
